99564e7afe640f2838589a52acd116d3e2b0fb5c
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-03-06 13:08:51 -0500

    Made pencil work

--- Server6/public/javascripts/canvasScript.js
+++ Server6/public/javascripts/canvasScript.js
@@ -1,34 +1,64 @@
 var imageView;
 var context;
-var started = false;
+var tool;
 
 $(document).ready(function(){
 	//Variables
 	imageView = $("#imageView");
 	context = imageView[0].getContext('2d');
+	tool = new pencil();
 
 	//Events
 	
 	//http://stackoverflow.com/questions/5751458/getting-correct-mouse-position-in-a-css-scaled-canvas
-	
+
+	imageView.mousemove(cursorEvent);
+	imageView.mousedown(cursorEvent);
+	imageView.mouseup(cursorEvent);
 
 	/**
-	 * Mousemove
-	 * @param  {Event} e 
+	 * Does everything
 	 */
-	imageView.mousemove(function(ev){
+	function cursorEvent(ev){
 		var x = ev.pageX - imageView.offset().left;
 		var y = ev.pageY - imageView.offset().top;
-
-		if(!started){
-			//place initial position
-			context.beginPath();
-			context.moveTo(x, y);
-			started = true;
-		} else {
-			//draw
-			context.lineTo(x, y);
-			context.stroke();
+		var func = tool[ev.type];
+		if(func){
+			func(x, y);
 		}
-	});
+	}
 });
+
+
+/**
+ * Pencil Object
+ */
+function pencil(){
+	var tool = this;
+	this.started = false;
+
+	//Mouseup
+	this.mouseup = function(x, y){
+		if (tool.started) {
+	      tool.mousemove(x, y);
+	      tool.started = false;
+	    };
+	};
+
+
+	//Mousedown
+	this.mousedown = function(x, y){
+		context.beginPath();
+		context.moveTo(x, y);
+		tool.started = true;
+	};
+
+
+	//mousemove
+	this.mousemove = function(x, y){
+		if (tool.started) {
+	      context.lineTo(x, y);
+	      context.stroke();
+	    }
+	};
+}
