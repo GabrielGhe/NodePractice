ccd08c30408adccb3fbec616aa1ec15275b12fd0
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-08-26 21:41:05 -0400

    Made it work in the browser

--- Server10Grunt/Gruntfile.js
+++ Server10Grunt/Gruntfile.js
@@ -2,39 +2,37 @@ module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        
-        concat: {
-            dist: {
-                src: ['public/javascripts/*.js'],
-                dest: 'public/javascripts/built.js',
-            },
-        },//End concat
+  grunt.initConfig({
+      concat: {
+          dist: {
+              src: ['public/javascripts/*.js'],
+              dest: 'public/javascripts/built.js',
+          },
+      },//End concat
 
 
-        clean: {
-            build: {
-                src: ["public/javascripts/built.js", "public/javascripts/built.min.js"]
-            }
-        },//End clean
+      clean: {
+          build: {
+              src: ["public/javascripts/built.js", "public/javascripts/built.min.js"]
+          }
+      },//End clean
 
 
-        uglify: {
-            options: {
-                mangle: {
-                    toplevel: true
-                },
-                compress: true
-            },
-            my_target: {
-                files: {
-                    'public/javascripts/built.min.js': ["public/javascripts/built.js"]
-                }
-            }
-        }//End uglify
+      uglify: {
+          options: {
+              mangle: {
+                  toplevel: true
+              },
+              compress: true
+          },
+          my_target: {
+              files: {
+                  'public/javascripts/built.min.js': ["public/javascripts/built.js"]
+              }
+          }
+      }//End uglify
 
-    });//End initConfig
+  });//End initConfig
 
-    grunt.registerTask('default', ['clean', 'concat', 'uglify']);
+  grunt.registerTask('default', ['clean', 'concat', 'uglify']);
 };
