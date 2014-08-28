ce7f66d40a0d6d89622f99f4e9991f5ccd65b33f
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-08-27 22:05:10 -0400

    Initial commit Sserver12Mongoose

---
+++ Server12Mongoose/app.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+
+
+
+//Mongoose connecting
+//---------------------------------------------
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');//db name
+mongoose.connection.on('error', function() {
+  console.error('✗ MongoDB Connection Error. Please make sure MongoDB is running.');
+});
+
+
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
