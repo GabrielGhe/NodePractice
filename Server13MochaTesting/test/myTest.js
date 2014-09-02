725d43e857916836a2cae0e14ef618753b2e35ff
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-01 21:44:01 -0400

    added some chai should in there

--- Server13MochaTesting/test/myTest.js
+++ Server13MochaTesting/test/myTest.js
@@ -1,16 +1,35 @@
-//var app = require('../app');
-var agent = require('supertest-as-promised');
+var app = require('../app');
+var agent = require('supertest-as-promised')(app);
 
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
+
 chai.should();
 chai.use(chaiAsPromised);
 
 
 describe('First test', function(){
-    it('should go to index', function(){
+    it('should go to index (using supertest)', function(done){
         agent
-            .get('http://localhost:3000')
-            .expect(200);
+            .get('/')
+            .expect(200)
+            .end(done);
+    });
+
+    it('should go to index (using supertest) --- version 2', function(done){
+        agent
+            .get('/')
+            .expect(200, done);
+    });
+
+    it('should not find the page (using supertest-as-promised)', function(done){
+        agent
+            .get('/gbeqwigbq')
+            .expect(404)
+            .then(function(res){
+                "works".should.equal("works");
+                "not works".should.not.equal("works");
+            })
+            .done(done);
     });
 });
