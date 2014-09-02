86f75efd677e22036f5366a79a80d8cd99e0b8a1
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-01 21:18:39 -0400

    Initial

---
+++ Server13MochaTesting/Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/**/*.js']
+            }
+        }
+    });
+
+    // now "grunt taskname" will run concat, uglify, some-other-task in that order
+    grunt.registerTask('default', ['mochaTest']);
+};
