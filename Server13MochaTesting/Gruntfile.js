4b0162dc5ee83ee3df2def5e6e3fa442dd6581f9
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-01 21:29:58 -0400

    not working...

--- Server13MochaTesting/Gruntfile.js
+++ Server13MochaTesting/Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
+    
     grunt.initConfig({
         mochaTest: {
             test: {
--- Server13MochaTesting/test/myTest.js
+++ Server13MochaTesting/test/myTest.js
@@ -1,8 +1,16 @@
+//var app = require('../app');
+var agent = require('supertest-as-promised');
+
 var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
 chai.should();
+chai.use(chaiAsPromised);
+
 
 describe('First test', function(){
     it('should go to index', function(){
-
+        agent
+            .get('http://localhost:3000')
+            .expect(200);
     });
 });
