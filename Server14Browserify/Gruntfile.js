182926409dbf1c71f76fd6763751f1a296f7ca73
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-03 21:50:14 -0400

    Now works very well

--- Server14Browserify/Gruntfile.js
+++ Server14Browserify/Gruntfile.js
@@ -4,7 +4,24 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.initConfig({
 
+        // will clean
+        clean: {
+            build: {
+                src: ['public/javascripts/build.js']
+            }
+        },
+
+        // will browserify all the js files
+        browserify: {
+            client: {
+                src: ['public/javascripts/**/*.js'],
+                dest: 'public/javascripts/build.js'
+            }
+        }
+
+        // will browserify bower libs
+        
     });
 
-    grunt.registerTask('default', ['']);
+    grunt.registerTask('default', ['clean', 'browserify:client']);
 };
--- Server14Browserify/public/javascripts/index.js
+++ Server14Browserify/public/javascripts/index.js
@@ -1,3 +1,4 @@
+var $ = require('jquery');
 var unique = require('uniq');
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
