aee8c079934436f6ac57d5608668bb6cc5658428
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-03 21:59:20 -0400

    added browserify-shim

--- Server14Browserify/Gruntfile.js
+++ Server14Browserify/Gruntfile.js
@@ -4,10 +4,18 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.initConfig({
 
+        // Add the lib to the main file
+        concat: {
+            dist: {
+                src: ['public/javascripts/lib.js', 'public/javascripts/build.js'],
+                dest: 'public/javascripts/dist.js'
+            }
+        },
+
         // will clean
         clean: {
             build: {
-                src: ['public/javascripts/build.js']
+                src: ['public/javascripts/build.js', 'public/javascripts/lib.js']
             }
         },
 
@@ -17,11 +25,15 @@ module.exports = function(grunt) {
                 src: ['public/javascripts/**/*.js'],
                 dest: 'public/javascripts/build.js'
             }
-        }
+        },
 
         // will browserify bower libs
-        
+        browserifyBower: {
+            options: {
+                file: 'public/javascripts/lib.js'
+            }
+        }
     });
 
-    grunt.registerTask('default', ['clean', 'browserify:client']);
+    grunt.registerTask('default', ['clean', 'browserifyBower', 'browserify:client', 'concat']);
 };
