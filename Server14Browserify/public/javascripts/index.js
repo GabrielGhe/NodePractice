edb163f3327e82f2798c5a9c4052837c0a4d9ec5
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-05 10:31:28 -0400

    forgot something

--- Server14Browserify/public/javascripts/index.js
+++ Server14Browserify/public/javascripts/index.js
@@ -1,9 +1,11 @@
 "use strict";
 
-var unique = require('uniq');
-var $ = require("jquery");
+var unique = require('uniq');               // node module
+var $ = require('jquery');                  // bower module
+var sayMyName = require('./sayMyName');     // own module
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
-console.log($("body").html());
-console.log(unique(data));
+console.log("'Uniq' node module", unique(data));
+console.log("'jQuery' bower module", $('body').html());
+console.log("'sayMyName', own module", sayMyName());
