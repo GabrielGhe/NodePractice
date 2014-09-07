ebf9841a7e334d2c421773bd51634941d2a51ce7
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-07 18:38:48 -0400

    trying to figure out what I have to extract

--- Server16WebScraper/routes/index.js
+++ Server16WebScraper/routes/index.js
@@ -3,7 +3,22 @@
 var cheerio = require("cheerio")
   , request = require("request")
   , fs = require("fs");
+  , _ = require("underscore");
 
 exports.index = function(req, res){
-    
+  // http://fcms.concordia.ca/fcms/asc002_stud_all.aspx
+  // http://www.concordia.ca/encs/students/course-schedules/fall-2014-2015.html
+  
+  var url = "http://www.concordia.ca/encs/students/course-schedules/fall-2014-2015.html";
+
+  request(url, function(e, response, html) {
+    var $ = cheerio.load(html);
+    var tbody = $('#CSE').next().find("tbody");
+    var rows = tbody.find("tr");
+
+
+
+    // Get name + link
+    // Get title
+  });
 };
