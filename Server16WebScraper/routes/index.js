e0353aee830d2a9b990da935cbb5f5b08045f348
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-07 19:13:56 -0400

    cheerio and request didn't work because the js was loading the page, so I'm using selenium and grunt

--- Server16WebScraper/routes/index.js
+++ Server16WebScraper/routes/index.js
@@ -1,27 +1,8 @@
 "use strict";
 
-var cheerio = require("cheerio")
-  , request = require("request")
-  , fs = require("fs");
+var fs = require("fs")
   , _ = require("underscore");
 
 exports.index = function(req, res){
-  // http://fcms.concordia.ca/fcms/asc002_stud_all.aspx
-  // http://www.concordia.ca/encs/students/course-schedules/fall-2014-2015.html
   
-  var url = "http://www.concordia.ca/encs/students/course-schedules/fall-2014-2015.html";
-
-  request(url, function(e, response, html) {
-    var $ = cheerio.load(html);
-    var tbody = $('#CSE').next().find("tbody");
-    var rows = tbody.find("tr");
-
-    _.each(rows, function(row) {
-      var nameAnchor = row.find("th").find("a");
-      var link = nameAnchor.href;
-      var name = nameAnchor.text();
-
-      console.log(name + " " + link);
-    });
-  });
 };
