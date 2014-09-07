09b39a5c76c0b4c505a352f678fe5dafb0db71f7
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-07 19:26:22 -0400

    am going to try out selenium with grunt-mocha-webdriver

--- Server16WebScraper/Gruntfile.js
+++ Server16WebScraper/Gruntfile.js
@@ -4,8 +4,26 @@ module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
   grunt.initConfig({
-    
+    mochaWebdriver: {
+      options: {
+        timeout: 1000 * 60 * 3,
+        reporter: 'spec'
+      },
+      chrome: {
+        src: ['scrape/index.js'],
+        options: {
+          testName: 'scrapping',
+          hostname: '127.0.0.1',
+          port:   '4444',
+          usePromises: true,
+          autoInstall: true,
+          browsers: [
+            {browserName: 'chrome'}
+          ]
+        }
+      }
+    }// end mochaWebdriver
   });
 
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ["mochaWebdriver:chrome"]);
 };
--- Server16WebScraper/scrape/index.js
+++ Server16WebScraper/scrape/index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+var browser
+  , wd;
+
+describe("Scraping", function() {
+  
+  // Getting the globals
+  before(function(done) {
+    browser = this.browser;
+    wd = this.wd;
+  });
+
+  it("should just go to the url and wait", function() {
+    browser.get("http://www.concordia.ca/encs/students/course-schedules/fall-2014-2015.html");
+  });
+});
