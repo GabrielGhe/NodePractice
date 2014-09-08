c25ddbcc36dadb8ed8c063d65fbe2080363fa7b9
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-07 20:11:37 -0400

    Made it go to the right site and change to comp engineering in fall

--- Server16WebScraper/scrape/index.js
+++ Server16WebScraper/scrape/index.js
@@ -1,17 +1,20 @@
 "use strict";
 
-var browser
-  , wd;
+var setSemesterFall = "document.getElementById(\"ctl00_PageBody_ddlSess\").value = \"2\"";
+var setEng = "document.getElementById(\"ctl00_PageBody_ddlDept\").value = \"04\"";
 
 describe("Scraping", function() {
-  
-  // Getting the globals
-  before(function(done) {
-    browser = this.browser;
-    wd = this.wd;
-  });
 
-  it("should just go to the url and wait", function() {
-    browser.get("http://www.concordia.ca/encs/students/course-schedules/fall-2014-2015.html");
+  it("should just go to the url and wait", function(done) {
+    // WD api
+    // https://github.com/admc/wd/blob/097719ac424a83d0a63b419691624997746f814a/doc/api.md
+
+    this.browser
+      .get("http://fcms.concordia.ca/fcms/asc002_stud_all.aspx")
+      .waitForElementByCss("#ctl00_PageBody_ddlSess", this.wd.asserters.isDisplayed)
+      .execute(setSemesterFall)
+      .execute(setEng)
+      .sleep(4000)
+      .done(done);
   });
 });
