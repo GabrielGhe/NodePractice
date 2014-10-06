58c55de2c76773eb9668d38f32d07e9d51409baf
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-10-06 00:43:12 -0400

    added routes

--- Server17Angular/public/javascripts/index.js
+++ Server17Angular/public/javascripts/index.js
@@ -1,3 +1,22 @@
 "use strict";
 
 var MyApp = angular.module("MyApp", ["ngRoute"]);
+
+MyApp.config(["$routeProvider", "$locationProvider",
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when("/:id", {
+        templateUrl: "partials/partialId.html",
+        controller: "idCtrl"
+      })
+      .when("/", {
+        templateUrl: "partials/home.html",
+        controller: "homeCtrl"
+      })
+      .otherwise({
+        redirectTo: "/"
+      });
+
+    $locationProvider.html5Mode(true);
+  }
+]);
