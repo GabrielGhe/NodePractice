7fbd3b4790188e8316c25ffbfe54c9266b53f6ed
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-10-14 11:40:05 -0400

    added personlist and person components

--- Server19ReactMore/public/javascripts/first.js
+++ Server19ReactMore/public/javascripts/first.js
@@ -4,7 +4,22 @@
 var PersonApp = React.createClass({displayName: 'PersonApp',
   render: function(){
     return (
-      React.DOM.h1(null, "React works")
+      React.DOM.div(null, 
+        React.DOM.h1(null, "This is the person App"),
+        Person( {first:"Vush", last:"Ky"} ),
+        Person( {first:"Dai", last:"Sy"} ),
+        Person( {first:"Ja", last:"Ky"} )
+      )
+    );
+  }
+});
+
+var Person = React.createClass({displayName: 'Person',
+  render: function(){
+    return (
+      React.DOM.div(null, 
+        this.props.first, " - ", this.props.last
+      )
     );
   }
 });
--- Server19ReactMore/public/javascripts/first.jsx
+++ Server19ReactMore/public/javascripts/first.jsx
@@ -1,12 +1,45 @@
 /*** @jsx React.DOM */
 "use strict";
 
+var data = [
+  {first:"Vush", last: "Ky"},
+  {first:"Dai", last: "Sy"},
+  {first:"Ja", last: "Ky"}
+];
+
 var PersonApp = React.createClass({
   render: function(){
     return (
-      <h1>React works</h1>
+      <div>
+        <h1>This is the person App</h1>
+        <PersonList data=this.props.data />
+      </div>
     );
   }
-});
+});// End PersonApp
 
-React.renderComponent(<PersonApp />, document.getElementById('first'));
+
+var PersonList = React.createClass({
+  render: function(){
+    var people = this.props.data.map(function(person){
+      return (
+        <Person first={person.first} last={person.last} />
+      );
+    });// End People
+
+
+  }
+});// End PersonList
+
+
+var Person = React.createClass({
+  render: function(){
+    return (
+      <div>
+        {this.props.first} - {this.props.last}
+      </div>
+    );
+  }
+});// End Person
+
+React.renderComponent(<PersonApp data={data} />, document.getElementById('first'));
