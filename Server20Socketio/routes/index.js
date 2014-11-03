99f4dff16b61038a54b9c3c08a2f04fabf8f5654
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-11-03 12:37:58 -0500

    added disconnect and chat message event on server side

--- Server20Socketio/routes/index.js
+++ Server20Socketio/routes/index.js
@@ -13,5 +13,16 @@ exports.init = function(io) {
   io.on('connection', function(socket){
     console.log('user connected ' + socket.id);
 
+    // Disconnect
+    socket.on('disconnect', function(){
+      console.log('user disconnected ' + socket.id);
+    });
+
+    // Chat Message
+    socket.on('chat message', function(msg){
+      console.log('user ' + socket.id + ': ' + msg);
+      // Send to everyone except for this socket
+      socket.broadcast.emit('Hello from ' + socket.id);
+    });
   });
 };
